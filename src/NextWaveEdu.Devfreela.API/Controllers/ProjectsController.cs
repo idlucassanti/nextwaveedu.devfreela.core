411866f898d6379971d74e89680be29fd053ad45
Lucas Santi <idlucassanti@hotmail.com>
2024-09-18 04:06:46 -0300

    #002 [API] Criação de exemplo dos endpoints para cada Verbo HTTP

---
+++ src/NextWaveEdu.Devfreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NextWaveEdu.Devfreela.API.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        // api/projects?query=aspnetcore
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        // api/projects/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+
+            //return NotFound();
+            return Ok();
+        }
+
+        // api/projecets
+        [HttpPost]
+        public IActionResult Post([FromBody] object input)
+        {
+            //return BadRequest();
+
+            // Cadastrar o Projeto
+
+            var projectCreatedId = 1;
+            return Created(nameof(GetById), new { projectCreatedId });
+        }
+
+        // api/projects/1
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] object updateInput)
+        {
+            // Atualizar o Projeot pelo Id
+
+            return NoContent();
+        }
+
+        // api/projects/1
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            //return NotFound();
+
+            // Remoção do id
+            return NoContent();
+        }
+    }
+}
