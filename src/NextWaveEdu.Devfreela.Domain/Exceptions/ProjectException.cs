ce36cf73108ffd156de5ec74ff5667bc3be09e88
Lucas Santi <idlucassanti@hotmail.com>
2024-09-20 14:21:52 -0300

    #007 [DOMAIN] Domain Exception da entidade Project

---
+++ src/NextWaveEdu.Devfreela.Domain/Exceptions/ProjectException.cs
@@ -0,0 +1,11 @@
+﻿namespace NextWaveEdu.Devfreela.Domain.Exceptions
+{
+    public class ProjectException : Exception
+    {
+        public ProjectException()
+            :base("Project is already in Started status")
+        {
+            
+        }
+    }
+}
