2953857ca9c7d9f3c16b96e221b7b31a27c17690
Lucas Santi <idlucassanti@hotmail.com>
2024-09-21 17:45:25 -0300

    #009 [INFRA] Interface de serviços Cloud

---
+++ src/NextWaveEdu.Devfreela.Infrastructure/CloudServices/Interfaces/IFileStorageService.cs
@@ -0,0 +1,7 @@
+﻿namespace NextWaveEdu.Devfreela.Infrastructure.CloudServices.Interfaces
+{
+    public interface IFileStorageService
+    {
+        void UploadFile(byte[] bytes, string fileName);
+    }
+}
