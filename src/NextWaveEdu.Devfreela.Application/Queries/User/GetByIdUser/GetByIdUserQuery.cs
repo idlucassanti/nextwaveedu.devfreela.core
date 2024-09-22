15c5260ef05ebd3cfe0f0aec257e91fa2c3a62f4
Lucas Santi <idlucassanti@hotmail.com>
2024-09-22 15:50:28 -0300

    #040 [APPLICATION] Criação das Queries de User do padrão CQRS

---
+++ src/NextWaveEdu.Devfreela.Application/Queries/User/GetByIdUser/GetByIdUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using NextWaveEdu.Devfreela.Application.ViewModels.User;
+
+namespace NextWaveEdu.Devfreela.Application.Queries.User.GetByIdUser
+{
+    public class GetByIdUserQuery : IRequest<UserViewModel>
+    {
+        public GetByIdUserQuery(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+    }
+}
---
+++ src/NextWaveEdu.Devfreela.Application/Queries/User/GetByIdUser/GetByIdUserQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using NextWaveEdu.Devfreela.Application.ViewModels.User;
+using NextWaveEdu.Devfreela.Infrastructure.Persistence;
+
+namespace NextWaveEdu.Devfreela.Application.Queries.User.GetByIdUser
+{
+    public class GetByIdUserQueryHandler : IRequestHandler<GetByIdUserQuery, UserViewModel>
+    {
+        private readonly DevfreelaDbContext _dbContext;
+
+        public GetByIdUserQueryHandler(DevfreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<UserViewModel> Handle(GetByIdUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = _dbContext.Users.SingleOrDefault(x => x.Id == request.Id);
+
+            if (user is null)
+                return null;
+
+            return new UserViewModel(user.Id, user.Name, user.Email, user.BirthDate);
+        }
+    }
+}
